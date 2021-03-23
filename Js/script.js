8f248c862da4f44916960841c2d8260d9527885c
FirephoenixX02 <otis.schmedt@gmx.de>
2021-03-23 10:25:11 +0100

    Some new features

--- Js/script.js
+++ Js/script.js
@@ -11,7 +11,9 @@ class Calculator {
     this.operation = undefined;
   }
 
-  delete() {}
+  delete() {
+    this.currentOperant = this.currentOperant.slice(0, -1);
+  }
 
   appendNumber(number) {
     if (number === "." && this.currentOperant.includes(".")) return;
@@ -30,11 +32,47 @@ class Calculator {
     this.currentOperant = "";
   }
 
-  compute() {}
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperant);
+    const current = parseFloat(this.currentOperant);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "÷":
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    this.currentOperant = computation;
+    this.operation = undefined;
+    this.previousOperant = "";
+  }
+
+  getDisplayNumber(number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    const floatNumber = parseFloat(number)
+    if (isNaN(floatNumber)) return ''
+    return floatNumber.toLocaleString('en')
+  }
 
   updateDisplay() {
     this.currentOperantTextElement.innerText = this.currentOperant;
-    this.previousOperantTextElement.innerText = this.previousOperant;
+    if (this.operation != null) {
+      this.previousOperantTextElement.innerText =
+       `${this.getDisplayNumber(this.previousOperant)} ${this.operation}`;
+    }
   }
 }
 
@@ -64,7 +102,26 @@ numberButtons.forEach((button) => {
 
 operationButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    calculator.appendNumber(button.innerText);
+    calculator.chooseOperation(button.innerText);
     calculator.updateDisplay();
   });
+});
+
+equalsButton.addEventListener("click", (button) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+//FIXME: Equals button does nothing.
+
+allClearButton.addEventListener("click", (button) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+//FIXME: AllClear Button(AC) only stops Input not removes the Numbers
+
+deleteButton.addEventListener("click", (button) => {
+  calculator.delete();
+  calculator.updateDisplay();
 });
